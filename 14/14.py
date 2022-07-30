ddb865ab3ae45803092bb6e4eef7e3e64842bd54
Moxitron <70322560+moxitron@users.noreply.github.com>
2022-07-30 12:57:52 +0430

    Solved Day 14

---
+++ 14/14.py
@@ -0,0 +1,33 @@
+#! /bin/python3 
+import re 
+import sys
+from collections import defaultdict , Counter
+
+def run(n , template , rules) : 
+	
+	chars = defaultdict(int , Counter(template))
+	template = Counter(zip(template , template[1:])) 
+	
+	for _ in range(n) : 
+		temp = defaultdict(int) 
+		for pair , count in template.items() : 
+			chars[rules[pair]] += count 	
+			temp[pair[0] , rules[pair]] += count	
+			temp[rules[pair] , pair[1]] += count	
+
+
+		template = temp
+	return max(chars.values()) - min(chars.values()) 
+		
+
+
+with open(sys.argv[1]) as f : 
+	template , rules = f.read().split('\n\n') 	
+
+
+rules = re.findall(r'(\w+) -> (\w+)' , rules)
+rules = { tuple(pair) : value for pair , value in rules } 
+print(run(10 , template , rules)) 
+print(run(40 , template , rules)) 
+
+
