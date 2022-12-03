50c4fd31c4831b8c2a35b640df1180be86727f33
Moxitron <70322560+moxitron@users.noreply.github.com>
2022-12-03 16:40:45 +0330

    solved day 3

--- 2022/3/3.py
+++ 2022/3/3.py
@@ -1,9 +1,7 @@
+from string import ascii_letters
 
 def priority(s) : 	
-	if s.islower() : 
-		return ord(s) - ord('a') + 1 
-	else :
-		return ord(s) - ord('A') + 27 
+	return ascii_letters.index(s) + 1 
 
 def shared(*args) : 
 	s = set.intersection(*[set(a) for a in args]).pop()
@@ -14,5 +12,3 @@ with open('in', 'r') as f :
 
 print(sum([shared(i[:len(i)//2], i[len(i)//2:]) for i in items]))
 print(sum([shared(*items[i: i+3]) for i in range(0, len(items), 3)]))
-
-
